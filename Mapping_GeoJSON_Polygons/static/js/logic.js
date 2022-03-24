eb977c308932c0bc8a5494f5520378067d6f2709
ntorenduwayo <jpnduwntore@gmail.com>
2022-03-24 12:29:38 -0700

    Adding Mapping_GeoJSON_Polygons files

---
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -0,0 +1,64 @@
+// Add console.lo to check to see if our code is working.
+console.log("working");
+
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	accessToken: API_KEY
+});
+// We create the tile layer that will be the background of our map.
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+    "Streets": streets,
+    "Day Naviation": satelliteStreets
+};
+
+// Create the map object with a center, zoom level and default layer.
+let map = L.map('mapid', {
+    center: [43.7, -79.3],
+    zoom: 11,
+    layers: [satelliteStreets]
+});
+
+// Pass our map layers into our layer control and add the layer control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+// Accessing the Toronto neighborhoods GeoJSON URL.
+let torontoHoods = "https://raw.githubusercontent.com/ntorenduwayo/Mapping_Earthquakes/main/torontoNeighborhoods.json";
+//let torontoHoods = "C:/Users/jpndu/Class/Mapping_Earthquakes/torontoNeighborhoods.json"
+
+// // Grabbing our GeoJSON data.
+// d3.json(torontoHoods).then(function(data) {
+//     console.log(data);
+
+// // Creating a GeoJSON layer with the retrieved data.
+//   L.geoJSON(data).addTo(map);
+// });
+
+
+
+// Create a style for the lines.
+let myStyle = {
+   color: "blue",
+   fillColor: 'yellow',
+   weight: 1
+ }
+// Grabbing our GeoJSON data.
+d3.json(torontoHoods).then(function(data) {
+   console.log(data);
+
+// Creating a GeoJSON layer with the retrieved data & add a popup marker that displays all airports' codes and names.
+  L.geoJSON(data, {
+    style: myStyle,
+    onEachFeature: function(feature, layer) {
+      console.log(layer);
+      layer.bindPopup("<h3> Neighborhood: " + feature.properties.AREA_NAME + "</h3>");
+    }
+  }).addTo(map);
+});
